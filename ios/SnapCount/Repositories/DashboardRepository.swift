1188eacfb97eb11bff647577a41f67e4ada36f6a
ylc <3518499943@qq.com>
2026-07-12 19:42:05 +0800

    refactor: 拆分 Dashboard 远端服务职责

--- ios/SnapCount/Repositories/DashboardRepository.swift
+++ ios/SnapCount/Repositories/DashboardRepository.swift
@@ -4,10 +4,26 @@ protocol DashboardRepositoryProtocol {
     func fetchDashboard(accessToken: String) async throws -> DashboardSnapshot
 }
 
-final class DashboardRepository: DashboardRepositoryProtocol {
-    private let remoteService: NativeDataService
+protocol DashboardRemoteServiceProtocol {
+    func fetchDashboard(accessToken: String) async throws -> DashboardSnapshot
+}
 
-    init(remoteService: NativeDataService = NativeDataService()) {
+final class DashboardRemoteService: DashboardRemoteServiceProtocol {
+    private let legacyService: NativeDataService
+
+    init(legacyService: NativeDataService = NativeDataService()) {
+        self.legacyService = legacyService
+    }
+
+    func fetchDashboard(accessToken: String) async throws -> DashboardSnapshot {
+        try await legacyService.fetchDashboard(accessToken: accessToken)
+    }
+}
+
+final class DashboardRepository: DashboardRepositoryProtocol {
+    private let remoteService: DashboardRemoteServiceProtocol
+
+    init(remoteService: DashboardRemoteServiceProtocol = DashboardRemoteService()) {
         self.remoteService = remoteService
     }
 
